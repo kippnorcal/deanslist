6ffaec52fdcdefc42cd7c097fa57d6f8a744eca7
Susan Xiong <susanxiong@kbas-c1mwj1fkj1wk.attlocal.net>
2020-01-02 11:49:12 -0800

    PROJ-1092 Use requests params instead of hardcoding into URL

--- api.py
+++ api.py
@@ -11,10 +11,13 @@ class API:
         self.api_key = api_key
         self.version = version
 
-    def get(self, entity):
+    def get(self, entity, addl_params=None):
+        params = {"apikey": self.api_key}
+        if addl_params:
+            params.update(addl_params)
         if self.version == "v1":
-            url = f"{DOMAIN}/api/v1/{entity}?apikey={self.api_key}"
+            url = f"{DOMAIN}/api/v1/{entity}"
         elif self.version == "beta":
-            url = f"{DOMAIN}/api/beta/export/{entity}.php?apikey={self.api_key}"
-        response = requests.get(url)
+            url = f"{DOMAIN}/api/beta/export/{entity}.php"
+        response = requests.get(url, params=params)
         return response.json()
