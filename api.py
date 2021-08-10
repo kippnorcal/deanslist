ffdb145f14a0cfb25588dcedab78b1b5b44a4d60
Susan Xiong <48801993+susanxiong@users.noreply.github.com>
2021-08-10 16:47:31 -0700

    PROJ22-578 Black autoformat

--- api.py
+++ api.py
@@ -9,7 +9,7 @@ DOMAIN = getenv("DOMAIN")
 class API:
     def __init__(self, version, api_key):
         """Initialize the class with the API key (school-specific) and URL.
-        
+
         Certain endpoints are only available on the beta version of the API
         (eg. behaviors, communications).
         """
@@ -18,7 +18,7 @@ class API:
 
     def get(self, entity, addl_params=None):
         """Get the response for this endpoint.
-        
+
         entity: the keyword for the endpoint (defined by DeansList API documentation)
         addl_params: dictionary of additional parameters, such as start/end date
             eg. {"sdt": "2019-12-01, "edt": "2019-12-31"}
